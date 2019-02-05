c7d47f0c0014e4ec774425cc5cefe13d1dce8e01
mitodz <mitodzzz@gmail.com>
2019-02-05 08:50:31 +0300

    Похоже решил, но наивным способом

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in).useDelimiter("\\W+");
+        Scanner scanner = new Scanner("2 15\n" +
+                "z: 0\n" +
+                "y: 10\n" +
+                "000001010101010").useDelimiter("\\W+");
         long n = scanner.nextInt(); // количество уникальных букв
         long m = scanner.nextInt(); // количество символов результирующей строки
         if (n == 1) {
@@ -19,8 +22,7 @@ public class Main {
                 map.put(scanner.next(), scanner.nextInt());
             }
             code = scanner.next(); //закодированная строка
-
-            for (int i = 0; i <= code.length(); i++) {
+            for (int i = 0; i <= m; i++) {
                 map.forEach((x, y) -> {
                     if (code.startsWith(y.toString())) {
                         System.out.print(x);
