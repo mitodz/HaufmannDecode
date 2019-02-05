1c1290ec5085d8cd768ab896a1fbc9bc42ece8b5
mitodz <mitodzzz@gmail.com>
2019-02-05 08:41:21 +0300

    Похоже решил, но наивным способом

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner("4 14\n" +
-                "a: 0\n" +
-                "b: 10\n" +
-                "c: 110\n" +
-                "d: 111\n" +
-                "01001100100111").useDelimiter("\\W+");
-
+        Scanner scanner = new Scanner(System.in).useDelimiter("\\W+");
         long n = scanner.nextInt(); // количество уникальных букв
         long m = scanner.nextInt(); // количество символов результирующей строки
-        Map<String, Integer> map = new LinkedHashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(scanner.next(), scanner.nextInt());
-        }
-        code = scanner.next(); //закодированная строка
-        for (int i = 0; i < code.length(); i++) {
-            map.forEach((x,y)-> {
-                if (code.startsWith(y.toString())) {
-                    System.out.print(x);
-                    code=code.subSequence(1,code.length()-1).toString();
-                }
-            });
+        if (n == 1) {
+            String s = scanner.next();
+            for (int i = 0; i < m; i++) {
+                System.out.print(s);
+            }
+        } else {
+            Map<String, Integer> map = new LinkedHashMap<>();
+            for (int i = 0; i < n; i++) {
+                map.put(scanner.next(), scanner.nextInt());
+            }
+            code = scanner.next(); //закодированная строка
+
+            for (int i = 0; i <= code.length(); i++) {
+                map.forEach((x, y) -> {
+                    if (code.startsWith(y.toString())) {
+                        System.out.print(x);
+                        code = code.subSequence(y.toString().length(), code.length()).toString();
+                    }
+                });
+            }
         }
     }
 }
